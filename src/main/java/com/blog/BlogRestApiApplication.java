1e297382e660ac95cdf5ad1a753ad961eaa660e6
Crisnzx <cristovaon13@gmail.com>
2023-03-08 11:26:33 -0300

    feat: Add environment variables

--- src/main/java/com/blog/BlogRestApiApplication.java
+++ src/main/java/com/blog/BlogRestApiApplication.java
@@ -1,20 +1,38 @@
 package com.blog;
 
+import com.blog.entities.Role;
+import com.blog.repositories.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BlogRestApiApplication {
+public class BlogRestApiApplication implements CommandLineRunner {
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    @Autowired
+    private RoleRepository roleRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogRestApiApplication.class, args);
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BlogRestApiApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String[] ROLES = new String[]{"ROLE_ADMIN", "ROLE_USER"};
+
+        for (String roleName : ROLES) {
+            Role role = new Role();
+            role.setName(roleName);
+            this.roleRepository.save(role);
+        }
+
+    }
 }
